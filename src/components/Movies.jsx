4046d5525e5009176485346fb0c211a2aa3f064c
Stas <stassmirnov115@gmail.com>
2023-01-24 17:30:23 +0700

    env and loading

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Movie from "./Movie";
 
-function Movies({ movies }) {
+function Movies({ movies = [] }) {
   return (
     <div className="flex flex-wrap justify-center gap-4">
-      {movies.map((obj) => (
-        <Movie {...obj} key={obj.imdbID} />
-      ))}
+      {movies.length ? (
+        movies.map((obj) => <Movie {...obj} key={obj.imdbID} />)
+      ) : (
+        <h3>Nothing found</h3>
+      )}
     </div>
   );
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,28 +1,37 @@
 import React from "react";
+
 import Movies from "../components/Movies";
 import Preloader from "../components/Preloader";
 import Search from "../components/Search";
 
+const API_KEY = import.meta.env.VITE_APP_KEY;
+
 function Main() {
   const [movies, setMovies] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=8ddc5ad2&s=matrix`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+      .then((data) => setMovies(data.Search), setLoading(false));
   }, []);
 
   const searchMovies = (str, filterType = "all") => {
-    fetch(`http://www.omdbapi.com/?apikey=8ddc5ad2&s=${str}${filterType !== 'all' ? `&type=${filterType}` : ''}`)
+    setLoading(true);
+    fetch(
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+        filterType !== "all" ? `&type=${filterType}` : ""
+      }`
+    )
       .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+      .then((data) => setMovies(data.Search), setLoading(false));
   };
 
   return (
     <main className="grow container mx-auto py-8">
       <Search searchMovies={searchMovies} />
       <div className="py-8">
-        {movies?.length ? <Movies movies={movies} /> : <Preloader />}
+        {loading ? <Preloader /> : <Movies movies={movies} />}
       </div>
     </main>
   );
